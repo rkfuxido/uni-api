6d2863627923804b107662a9c893554913bb93bf
rkfuxido <165876309+rkfuxido@users.noreply.github.com>
2024-12-10 09:11:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2146,7 +2146,7 @@ if __name__ == '__main__':
     uvicorn.run(
         "__main__:app",
         host="0.0.0.0",
-        port=8000,
+        port=80,
         reload=True,
         reload_dirs=["./"],
         reload_includes=["*.py", "api.yaml"],
